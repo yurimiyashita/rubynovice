80c0feb0b380dafb79d7eeb6fb615aacb75a68cc
Shinya Morishita <MorishitaShinya@morishita-no-MacBook-Air2.local>
2016-05-17 17:06:37 +0900

    rubynovice_spec.rbのEx1分を書き込みました．どのようなコードを書いて，実行される事を期待しているのかもコメントアウトで軽く書き込んであります．

--- spec/rubynovice_spec.rb
+++ spec/rubynovice_spec.rb
@@ -11,5 +11,20 @@ describe Rubynovice do
     print Rubynovice.ex1
     expect(Rubynovice.ex1).to eq(expected)
   end
+  #ここは普通にreturn "hello world.\n"として作成してくれるのを期待する．
+
+  it 'ex2 return 1+1' do
+    expected = 2
+    expect(Rubynovice.ex2).to eq(expected)
+  end
+  #1+1を変数に代入するかもしくはそのまま返されるのを期待する．
+  
+  it 'ex3 printf ans=2\n' do
+    expected = "ans=2\n"
+    expect(Rubynovice.ex3).to eq(expected)
+  end
+  #出力として変数に"ans=2\n"を代入してprintfで出力させて，
+  #その変数をreturnするようにするとprintfの使い方とあわせて
+  #テスト駆動させている感じにみえる．
 
 end
