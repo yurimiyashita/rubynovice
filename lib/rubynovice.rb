01c14dd90362cc5968a3e2b943e20504665be09f
Shigeto R. Nishitani <shigeto_nishitani@me.com>
2016-05-25 14:00:10 +0900

    rubynovice.rbでちゃんと辿れるようにpathを修正．

--- lib/rubynovice.rb
+++ lib/rubynovice.rb
@@ -1,10 +1,9 @@
 require "rubynovice/version"
 $LOAD_PATH.unshift File.expand_path("../../lib/#{ENV['RUBYNOVICE_NAME']}", __FILE__)
 begin
-  require "daddy/daddy"
-  require "donkey/donkey"
+  require 'ex_files'
 rescue LoadError
-  p "Load Error of daddy in rubynovice.rb."
+  p "Load Error of ex_files in rubynovice.rb."
   p File.expand_path("../../lib/#{ENV['RUBYNOVICE_NAME']}", __FILE__)
   exit
 end
