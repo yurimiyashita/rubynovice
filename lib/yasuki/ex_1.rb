69b924d627d3f6357d6639741469a992b06cea3f
yasuki0 <fky15330@kwansei.ac.jp>
2016-07-06 14:58:51 +0900

    first commit (#15)

---
+++ lib/yasuki/ex_1.rb
@@ -0,0 +1,18 @@
+class Rubynovice
+
+  def ex1_1
+    return "hello world.\n"
+  end
+
+  def ex1_2
+    return 1+1
+  end
+
+  def ex1_3
+    ans = 1+1
+    text = sprintf("ans=%i\n",ans)
+    return text
+  end
+
+
+end
