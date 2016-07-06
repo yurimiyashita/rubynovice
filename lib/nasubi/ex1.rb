a85ffbb6a3820e2c1802dc9f162f5b207401f9c0
nasu14 <bta37052@kwansei.ac.jp>
2016-07-06 14:58:36 +0900

    first commit (#14)

---
+++ lib/nasubi/ex1.rb
@@ -0,0 +1,31 @@
+class Rubynovice
+
+  def ex1_1
+   puts "Hello world!"
+
+    return "Hello world!"
+
+  end
+
+
+
+  def ex1_2
+
+    return 1+1
+    
+  end
+ 
+  def ex1_3
+
+    printf( "ans=2\\n\n" )
+
+    return "ans=2\\n\n"
+
+  end
+
+end
+
+Rubynovice.new.ex1_1
+Rubynovice.new.ex1_3
+
+
